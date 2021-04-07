9139aefc108857215b11564d72fd42ed4c35089c
Konrad <konrad.hajduga@gmail.com>
2021-04-07 10:49:30 +0200

    upgrade constraints

--- EinsteinRiddle/secondProblem/einstein_riddle_constraint.py
+++ EinsteinRiddle/secondProblem/einstein_riddle_constraint.py
@@ -6,12 +6,24 @@ from secondProblem.sentence import Sentence
 
 
 class RiddleConstraint(Constraint[Sentence, Sentence]):
-    def __init__(self, first_sentence, second_sentence):
+    def __init__(self, first_sentence, second_sentence, neighbours=False, left_side_neighbour=None):
         super().__init__([first_sentence, second_sentence])
         self.first_sentence = first_sentence
         self.second_sentence = second_sentence
+        self.neighbours = neighbours
+        self.left_side_neigbour = left_side_neighbour
 
     def satisfied(self, assignment: Dict[Sentence, House]):
+        # jesli sasiedzi to sprawdz czy domy sa obok (numery) ale po co mi to sprawdzac tutaj?
+        # jesli to juz sa przypisane
+        if self.neighbours and self.first_sentence in assignment and self.second_sentence in assignment:
+            if self.left_side_neigbour is None:
+                if abs(assignment[self.first_sentence].number - assignment[self.second_sentence].number) != 1:
+                    return False
+            elif self.left_side_neigbour:
+                if assignment[self.first_sentence].number - assignment[self.second_sentence].number != -1:
+                    return False
+
         # czy powtarza sie dom dla takich samych typow (koloru, narodowosci itd)
         for s in assignment:
             for a in assignment:
